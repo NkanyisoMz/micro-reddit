40d5d20f24c643f2aacd31cb5ab82a5f37c7eda6
NkanyisoMz <104601010+NkanyisoMz@users.noreply.github.com>
2025-02-04 14:45:54 +0200

    Added validations on post.rb and established and update associations in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
   validates :email, presence: true, uniqueness: true
 end
