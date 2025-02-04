84ed4d1716508d0a8da4942ba44ea87dd2091c07
NkanyisoMz <104601010+NkanyisoMz@users.noreply.github.com>
2025-02-04 14:55:05 +0200

    Added comment model, added validated, and updated associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :body, presence: true
+  has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
   validates :email, presence: true, uniqueness: true
+  has_many :comments, dependent: :destroy
 end
